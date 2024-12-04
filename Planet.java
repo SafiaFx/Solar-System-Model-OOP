dcb799c670e171f4891f012b3599d1e7c6f0a71b
SafiaFx <151640065+SafiaFx@users.noreply.github.com>
2024-12-04 13:06:06 +0000

    Add files via upload

---
+++ Planet.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Planet {
+    // Class variables
+    private String planetName;
+    private double distance;
+
+    // Getters and setters
+    public String getPlanetName() {
+        return planetName;
+    }
+
+    public void setPlanetName(String name) {
+        this.planetName = name;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    // Constructor
+    public Planet(String name, double distance) {
+        this.planetName = name;
+        this.distance = distance;
+    }
+
+    // Method for calculating orbital period
+    public static double orbitalPeriod(double distance) {
+        return Math.sqrt(distance * distance * distance);
+    }
+}
--- SolarSystem.java
+++ SolarSystem.java
@@ -1,3 +1,55 @@
-public class SolarSystem{
-    
+import java.util.ArrayList;
+
+public class SolarSystem {
+
+    // Class variables
+    private String name;
+    // array list for planet objects
+    private ArrayList<Planet> planets;
+
+    // Constructor
+    public SolarSystem(String name) {
+        this.name = name;
+        this.planets = new ArrayList<>();
+    }
+
+    // Getters and setters
+    public ArrayList<Planet> getPlanets() {
+        return planets;
+    }
+
+    public void setPlanets(ArrayList<Planet> planets) {
+        this.planets = planets;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Method to print Solar system info
+    public void toString(String name,
+            ArrayList<Planet> planets) {
+        for (int i = 0; i < planets.size(); i++) {
+            System.out.println("Star" + name + "has planets: ");
+            // Get the planet object
+            Planet planet = planets.get(i);
+            // calculate planet period
+            double period = Planet.orbitalPeriod(planet.getDistance());
+            System.out
+                    .println(planet.getPlanetName() + " is " + planet.getDistance() + "AU from its star, and orbits in "
+                            + period + "years");
+        }
+    }
+
+    // Method for adding planets
+    public void addPlanet(String planetName, double distance) {
+        // Creation of Planet object
+        Planet planetX = new Planet(planetName, distance);
+        // Add planet to array list
+        planets.add(planetX);
+    }
 }
